3c649ad0e97fa47f0e05a204db09a494fdc8126a
BrittaBahr <55857820+BrittaBahr@users.noreply.github.com>
2019-10-31 22:43:18 +0100

    Add files via upload

---
+++ Homework_Bootstrap_Bahr_Britta/bootstrapFunction.js
@@ -0,0 +1,23 @@
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("movingProgressbar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+} 
+
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();   
+});
